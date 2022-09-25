f0e25895582eb65118b6829358b845c3ad652939
Zenylog-1 <reactnative.1@zenylog.com>
2022-09-25 13:57:02 +0530

    image url chages

--- src/helpers/api_helper.js
+++ src/helpers/api_helper.js
@@ -5,7 +5,7 @@ import {ASYN_USER_AUTH} from '@utils'
 const STAGING = 'http://43.204.233.45/';
 const LOCAL_DEV = 'http://192.168.6.204:8002';
 const PROD = 'https://api.zenyq.com';
-export const REACT_APP_APP_URL = LOCAL_DEV; 
+export const REACT_APP_APP_URL = PROD; 
 // 'http://localhost:8000' 
 // 'http://43.204.233.45' 
 // process.env.REACT_APP_APP_URL;
