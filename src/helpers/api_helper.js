135d4fe20d37f7c54ceddf6049a6cd7a852d8401
vikram_zenylog <django.1@zenylog.com>
2022-10-03 20:40:48 +0530

    events added

--- src/helpers/api_helper.js
+++ src/helpers/api_helper.js
@@ -3,7 +3,7 @@ import {ASYN_USER_AUTH} from '@utils'
 
 //apply base url for axios
 const STAGING = 'http://43.204.233.45/';
-const LOCAL_DEV = 'http://192.168.174.204:8000';
+const LOCAL_DEV = 'http://192.168.60.204:8000';
 const PROD = 'https://api.zenyq.com';
 export const REACT_APP_APP_URL = LOCAL_DEV; 
 // 'http://localhost:8000' 
--- src/modules/dashboard/screen/Calendar/index.tsx
+++ src/modules/dashboard/screen/Calendar/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import moment from "moment";
 import { Container, Calender, Card, Sort, Modal, Primary } from "@components";
 import { useTranslation } from "react-i18next";
 import { useDispatch, useSelector } from "react-redux";
@@ -12,61 +13,65 @@ function Calendar() {
   const navigation = useNav();
   const dispatch = useDispatch();
   const [model, setModel] = useState(false);
-  const [daysHoliday,setDaysHoliday]=useState<any[]>([])
+  const [daysHoliday,]=useState<any[]>([])
   const { t, i18n } = useTranslation();
   const { calendarEvents } = useSelector((state: any) => state.EmployeeReducer);
 
   useEffect(() => {
     let params = {};
     dispatch(fetchCalendardetails({}));
+    calendarEvents?.days_holiday?.map((item:any)=>{
+      daysHoliday.push({title:item.title,date:item.day,color:"green"})});
+    
+    calendarEvents?.days_leave?.map((item:any)=>{
+      if(item.date_from)
+      {
+        daysHoliday.push({title:item.reason,start:item.date_from,end:item.date_to,color:"red"})
+      }
+      else{
+        daysHoliday.push({title:item.reason,date:item.day,color:"red"})
+      }
+    })
+
+    calendarEvents?.days_absent?.map((item:any)=>{
+      if(item.date_from)
+      {
+        daysHoliday.push({title:item.reason,start:item.date_from,end:item.date_to,color:"red"})
+      }
+      else{
+        daysHoliday.push({title:item.reason,date:item.day,color:"red"})
+      }
+    })
+
+
   }, []);
 
   const handleDateClick = (arg: any) => {
-    setModel(!model);
-    dispatch(getLeaveFromDate(arg.dateStr));
-  };
+    
 
-  // const fetchEvents=(info:any,successCallback:any)=>{
-  //   calendarEvents?.days_holiday?.map((item:any)=>({title:item.title,date:item.day,color:"red"}))
-  // }
-
-  useEffect(()=>{
-    calendarEvents?.days_holiday?.map((item:any)=>{setDaysHoliday([...daysHoliday,{title:item.title,date:item.day,color:"green"}])})
-  },[])
+let data = daysHoliday.filter((item)=>item.date === arg.dateStr)
 
 
-  console.log(calendarEvents)
+ if(data)
+ {
+   console.log("data",data)
+ }
+ else{
+      setModel(!model);
+      dispatch(getLeaveFromDate(arg.dateStr));
+ }
+
+  }
+
+
   return (
     <>
       <Container additionClass={"col-9 mt-5"}>
         <Card>
           <Calender
             dateClick={handleDateClick}
-            // events={[
-            //   {
-            //     title: "Fever",
-            //     start: "2022-09-01",
-            //     end: "2022-09-05",
-            //     color: "red",
-                
-            //   },
-            //   {
-            //     title: "Independance Day",
-            //     start: "2022-08-25",
-            //     color:"green"
-            //   },
-            //   {
-            //     title: "Christmas",
-            //     start: "2022-12-25",
-            //     color: "green",
-            //   },
-            //   {
-            //     title: "Pongal",
-            //     start: "2023-01-15",
-            //     color: "green",
-            //   }
-            // ]}
-              events={daysHoliday}
+              events={daysHoliday?.length>1? daysHoliday :[]}
+              
           />
         </Card>
 
