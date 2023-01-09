1353d827faf89cc2d3a5b33917f4aa1741454816
LAPTOP-KFIM4RIF\iniya <iniyan.e@leorainfotech.in>
2023-01-09 09:37:05 +0530

    webapp 1.08

--- src/modules/dashboard/container/Navbar/index.tsx
+++ src/modules/dashboard/container/Navbar/index.tsx
@@ -87,7 +87,7 @@ const Navbar = ({ }) => {
               })}
             </ul>
           </div>
-          <small className={"text-white text-version"}>Version: 1.05</small>
+          <small className={"text-white text-version"}>Version: 1.08</small>
         </div>
       </div>
 
