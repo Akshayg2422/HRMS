c987dab51ebd083ca97a62db4d2790db0553744a
purushothamanzenylog <reactjs.1@zenylog.com>
2023-02-11 02:01:24 +0530

    launch screen fix

--- src/screens/Zenylog_site/index.js
+++ src/screens/Zenylog_site/index.js
@@ -1,26 +1,28 @@
 import React, { useEffect } from "react";
-import { Contact,Features,Home,Navbar,Policy,Flowchart,AboutUs } from "./Container";
-import {hideLoader} from '../../store/loader/actions';
-import {useDispatch} from 'react-redux';
+import { Contact, Features, Home, Navbar, Policy, Flowchart, AboutUs, LaunchScreen, LaunchSuccessScreen } from "./Container";
+import { hideLoader } from '../../store/loader/actions';
+import { useDispatch } from 'react-redux';
+import { useSelector } from "react-redux";
 
-function ZenylogSite (props) {
+
+function ZenylogSite(props) {
 
     const dispatch = useDispatch()
 
     useEffect(() => {
         dispatch(hideLoader())
-    })
+    },[])
 
     return (
         <div>
-            <Navbar/>
-            <Home/>
-            <AboutUs/>
-            <Features/>
-            <Flowchart/>
-            <Contact/>
-            <Policy/>
-        </div>
+            <Navbar />
+            <Home />
+            <AboutUs />
+            <Features />
+            <Flowchart />
+            <Contact />
+            <Policy />
+        </div >
     )
 }
 export default ZenylogSite;
