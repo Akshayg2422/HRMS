02543297a631db377b6e1d7f1ce8aa803f6c5dc6
purushothamanzenylog <reactjs.1@zenylog.com>
2023-03-18 15:14:25 +0530

    screen added Essl

--- src/modules/auth/screen/EssiConfig/index.tsx
+++ src/modules/auth/screen/EssiConfig/index.tsx
@@ -1,6 +1,6 @@
-import { Card, Container, FormWrapper, InputText, Primary } from '@components'
+import { Card, Container, FormWrapper, InputText, Primary, Secondary } from '@components'
 import { useNav } from '@utils';
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useTranslation } from 'react-i18next';
 import { useDispatch } from 'react-redux';
 
@@ -10,16 +10,47 @@ function EssiConfig() {
   const { t } = useTranslation();
   let dispatch = useDispatch();
 
+  useEffect(() => {
+    getEsslConfigDetails()
+  }, [])
+
+  const getEsslConfigDetails = () => {
+    const params = {}
+    // dispatch()
+  }
 
   return (
     <>
       <Card>
-        <Container additionClass='text-right'>
+        <Container additionClass='d-flex justify-content-between'>
+          <h3>{t('ESSIConfig')}</h3>
           <Primary text={t('add')} />
         </Container>
       </Card>
       <Card additionClass='mt-4 col-xl-6'>
-
+        <Container>
+          <Container textAlign={"text-left"}>
+            <span>
+              {t('BaseUrl')}
+              {":"}&nbsp;&nbsp;
+              <span className="text-black">{''}</span>
+            </span>
+            <br />
+            <span >
+              {t("userName")}
+              {":"}&nbsp;&nbsp;
+              <span className="text-black">{''}</span>
+            </span>
+            <br />
+            <span>
+              {t('Password')}
+              {":"}&nbsp;&nbsp;
+              <span className="text-black">
+                {"**************"}
+              </span>
+            </span>
+          </Container>
+        </Container>
       </Card>
     </>
   )
