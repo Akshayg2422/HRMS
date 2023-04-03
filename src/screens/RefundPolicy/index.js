f8d348e72e49fcb89c38ab153fad3a8be288d55d
TAMILSELVAN\tamil_hfh9g6g <tamilselvan.intern@leorainfotech.in>
2023-04-03 17:40:15 +0530

    refund policy changes

--- src/screens/RefundPolicy/index.js
+++ src/screens/RefundPolicy/index.js
@@ -3,7 +3,7 @@ import React from "react";
 const RefundPolicy = () => {
     return (
 
-        <div class="container">
+        <div class="container py-4">
             <div class="header-body text-center mb-7">
                 <div class="justify-content-center row">
                     <h1 class="text-black">Refund Policy</h1>
@@ -12,36 +12,55 @@ const RefundPolicy = () => {
             <div>
                 <h4>Last Updated On: 03/04/2023</h4>
                 <h3>General:</h3>
-                <p className="text-justify ml-4">This Website with the URL of WWW.ZENQ.COM and the App (the “Website/Site” and the
-                    “App”) is owned and operated by MAPLEBELL PRIVATE LIMITED, having its registered
-                    office at 363, 19th Main Road, Rajaji Nagar, Bengaluru, Bengaluru Urban, Karnataka- 560010.
-                    By visiting our Website or App and accessing the information, services we provide, you
-                    understand and agree to accept and adhere to the terms and conditions as stated in the Terms and
-                    Conditions (please refer to the Terms and Conditions Policy link of this Website for more
-                    information).</p>
+                <div className="text-justify ml-4">
+                    <p className="mb-0">This Website with the URL of WWW.ZENQ.COM and the App (the “Website/Site” and the
+                        “App”) is owned and operated by MAPLEBELL PRIVATE LIMITED, having its registered
+                        office at 363, 19th Main Road, Rajaji Nagar, Bengaluru, Bengaluru Urban, Karnataka- 560010.
+                    </p>
+                    <p>By visiting our Website or App and accessing the information, services we provide, you
+                        understand and agree to accept and adhere to the terms and conditions as stated in the Terms and
+                        Conditions (please refer to the Terms and Conditions Policy link of this Website for more
+                        information).
+                    </p>
+                </div>
             </div>
 
             <div className="mt-5">
                 <h4 className="mt-2"> RETURN &amp; REFUND POLICY</h4>
                 <h3>Effective from: 03/04/2023</h3>
-                <p className="text-justify ml-4">Welcome to ZENYQ.COM, We appreciate your valuable time. Thank you for your availing of
-                    our Service. We hope you are happy with your purchase. However, if you are not completely
-                    satisfied with your purchase for any reason, you may return it to us for Refund. We are here to
-                    help.
-                    Note: In case your order for a service is rejected or canceled for any reason, including errors or
-                    omissions in the information you provide to us. Even after payment has been processed, we will
-                    issue a refund to you in the amount of the Purchase price.
-                    We are committed to providing our customers or clients with the highest quality services.
-                    However, on rare occasions, Services may be found to be faulty or deficient. In such cases, we
-                    offer return and refund facilities in accordance with this Return and Refund Policy.
-                    Please read this Policy before making a purchase on our website, so that you understand your
-                    rights as well as what you can expect from ZENYQ.COM if you are not happy with your
-                    purchase.
-                    Please see below for more information on our return policy.</p>
+                <div className="text-justify ml-4">
+                    <p className="mb-0">
+                        Welcome to ZENYQ.COM, We appreciate your valuable time. Thank you for your availing of
+                        our Service. We hope you are happy with your purchase. However, if you are not completely
+                        satisfied with your purchase for any reason, you may return it to us for Refund. We are here to
+                        help.
+                    </p>
+                    <p className="mb-0">
+                        Note: In case your order for a service is rejected or canceled for any reason, including errors or
+                        omissions in the information you provide to us. Even after payment has been processed, we will
+                        issue a refund to you in the amount of the Purchase price.
+                    </p>
+                    <p className="mb-0">
+                        We are committed to providing our customers or clients with the highest quality services.
+
+                    </p>
+                    <p className="mb-0">
+                        However, on rare occasions, Services may be found to be faulty or deficient. In such cases, we
+                        offer return and refund facilities in accordance with this Return and Refund Policy.
+                    </p>
+                    <p className="mb-0">
+                        Please read this Policy before making a purchase on our website, so that you understand your
+                        rights as well as what you can expect from ZENYQ.COM if you are not happy with your
+                        purchase.
+                    </p>
+                    <p className="mb-0">
+                        Please see below for more information on our return policy.
+                    </p>
+                </div>
             </div>
             <div className="mt-5">
                 <h3>RETURNING OF A SERVICE:</h3>
-                <ul>
+                <ul className="text-justify ml-2">
                     <li> The returns of Any Services availed through the ZENYQ App or Website must be
                         postmarked within 15 DAYS of the Invoice date.</li>
                     <li> To be eligible for a return, your return of a Service must be in unused condition, with all
@@ -59,12 +78,16 @@ const RefundPolicy = () => {
             </div>
             <div className="mt-5">
                 <h3>RETURN PROCESS:</h3>
-                <p>To return a Service, please email customer service at maplebell.official@gmail.com to obtain a
-                    Return Merchandise Authorization (RMA)/Return Request Number (RRN), if any. After
-                    receiving a RMA/RRN number, place the item securely in its original packaging and (include
-                    your proof of purchase / the return form provided / other), and mail your return to the following</p>
-                <h4> address:</h4>
+                <div className="text-justify ml-4">
+                    <p>To return a Service, please email customer service at maplebell.official@gmail.com to obtain a
+                        Return Merchandise Authorization (RMA)/Return Request Number (RRN), if any. After
+                        receiving a RMA/RRN number, place the item securely in its original packaging and (include
+                        your proof of purchase / the return form provided / other), and mail your return to the following
+                    </p>
+                </div>
+
                 <div>
+                    <h4> address:</h4>
                     <h4>ZENYQ</h4>
                     <h4> A digital unit of MAPLEBELL PRIVATE LIMITED</h4>
                     <h4>Attn:Returns</h4>
@@ -76,7 +99,7 @@ const RefundPolicy = () => {
                 </div>
                 <div className="mt-5">
                     <h3>REFUNDS:</h3>
-                    <ul>
+                    <ul className="text-justify ml-2">
                         <li>After receiving your return and inspecting the condition of your purchased Service, we
                             will process your return or modification. Please allow at least Fifteen [15] days from the
                             receipt of your item to process your return or modification.</li>
@@ -99,7 +122,7 @@ const RefundPolicy = () => {
                 </div>
                 <div className="mt-5">
                     <h3>DAMAGING DURING THE TRANSIT:</h3>
-                    <ul>
+                    <ul className="text-justify ml-2">
                         <li>Replacement can be made if the customer or client establishes that the Service was
                             delivered in defective format or has technical interruption within 48 hours of receipt of
                             the Service.</li>
@@ -111,11 +134,13 @@ const RefundPolicy = () => {
                 </div>
                 <div className="mt-5">
                     <h3>EXCEPTION:</h3>
-                    <p>The following Services cannot be returned or modified,
-                    </p>
-                    <p> Please Note: Notwithstanding the other provisions of this Policy, we may refuse to provide
-                        maintenance, replacement, or refund for a Service you purchased if:</p>
-                    <ul>
+                    <div className="text-justify ml-4">
+                        <p className="mb-0">The following Services cannot be returned or modified,
+                        </p>
+                        <p > Please Note: Notwithstanding the other provisions of this Policy, we may refuse to provide
+                            maintenance, replacement, or refund for a Service you purchased if:</p>
+                    </div>
+                    <ul className="text-justify ml-2">
                         <li>The service purchased is a service offered during sale or as a promotional item. (i.e.
                             Discounts, Giveaways etc.)</li>
                         <li>If the Service has been used.</li>
@@ -130,17 +155,19 @@ const RefundPolicy = () => {
                 </div>
                 <div className="mt-5">
                     <h3>PRODUCT CONTAINING YOUR DATA:</h3>
-                    <p>In some of the cases, your data may be stored in the device/accessories you are returning to us. It
-                        is strictly advised to take a backup and remove all of your data before returning the
-                        device/accessories. We are not responsible for any loss or misuse of your data.
-                    </p>
-                    <p>The Privacy of your data supplied to us during the return and refund procedure is also governed
-                        by our Privacy Policy, which can be accessed under the Privacy Policy head of our website.
-                    </p>
+                    <div className="text-justify ml-4">
+                        <p className="mb-0">In some of the cases, your data may be stored in the device/accessories you are returning to us. It
+                            is strictly advised to take a backup and remove all of your data before returning the
+                            device/accessories. We are not responsible for any loss or misuse of your data.
+                        </p>
+                        <p>The Privacy of your data supplied to us during the return and refund procedure is also governed
+                            by our Privacy Policy, which can be accessed under the Privacy Policy head of our website.
+                        </p>
+                    </div>
                 </div>
                 <div className="mt-5">
                     <h3>RESPONSE TIME:</h3>
-                    <ol type="1">
+                    <ol type="1" className="text-justify ml-2">
                         <li>
                             A refund is normally processed within 15 Business days after the completion of quality
                             checks of the Service or the Associated accessories returned.
@@ -158,27 +185,33 @@ const RefundPolicy = () => {
                 </div>
                 <div className="mt-5">
                     <h3>CANCELLATION OF RETURN REQUEST:</h3>
-                    <p>A return request of any Services offered by ZENYQ can be canceled by contacting customer
-                        care at +91 8342999555 or by writing mail to us at: maplebell.official@gmail.com.</p>
+                    <div className="text-justify ml-4">
+                        <p>A return request of any Services offered by ZENYQ can be canceled by contacting customer
+                            care at +91 8342999555 or by writing mail to us at: maplebell.official@gmail.com.</p>
+                    </div>
 
                 </div>
                 <div className="mt-5">
                     <h3>REFUSAL OF RETURN OR REFUND REQUEST:</h3>
-                    <p>We reserve the right to refuse or cancel any return request if such a request is not in compliance
-                        with this Policy or Applicable laws.</p>
+                    <div className="text-justify ml-4">
+                        <p>We reserve the right to refuse or cancel any return request if such a request is not in compliance
+                            with this Policy or Applicable laws.</p>
+                    </div>
 
                 </div>
                 <div className="mt-5">
                     <h3>CONTACT INFORMATION:</h3>
-                    <p>If you have any questions or comments about ZENYQ Terms of Service as outlined above, you
-                        can contact us at:</p>
-                    <p>ZENYQ.COM,</p>
-                    <p>A digital property of MAPLEBELL PRIVATE LIMITED</p>
-                    <p>363, 19th Main Road,</p>
-                    <p>Rajaji Nagar, Bengaluru,</p>
-                    <p>Bengaluru Urban, Karnataka- 560010.</p>
-                    <p>Email Address: maplebell.official@gmail.com</p>
-                    <p>Phone Number: +91 8342 999 555</p>
+                    <div className="text-justify ml-4">
+                        <p className="mb-0">If you have any questions or comments about ZENYQ Terms of Service as outlined above, you
+                            can contact us at:</p>
+                        <p className="mb-0">ZENYQ.COM,</p>
+                        <p className="mb-0">A digital property of MAPLEBELL PRIVATE LIMITED</p>
+                        <p className="mb-0">363, 19th Main Road,</p>
+                        <p className="mb-0">Rajaji Nagar, Bengaluru,</p>
+                        <p className="mb-0">Bengaluru Urban, Karnataka- 560010.</p>
+                        <p className="mb-0">Email Address: maplebell.official@gmail.com</p>
+                        <p className="mb-0">Phone Number: +91 8342 999 555</p>
+                    </div>
                 </div>
             </div>
         </div >
