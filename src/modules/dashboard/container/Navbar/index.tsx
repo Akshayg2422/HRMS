6c29957d4db76239beddc8fa349953dc445a3060
Jayakumar <jayakumar@leorainfotech.in>
2023-04-11 17:28:29 +0530

    web app 1.30

--- src/modules/dashboard/container/Navbar/index.tsx
+++ src/modules/dashboard/container/Navbar/index.tsx
@@ -171,7 +171,7 @@ const Navbar = ({ }) => {
               }
             </ul>
           </div>
-          <small className={"text-white text-version"}>Version: 1.27</small>
+          <small className={"text-white text-version"}>Version: 1.30</small>
         </div>
       </div>
 
