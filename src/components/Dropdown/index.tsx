ba7291c5e652207b9b08a0231470b6af90669f8d
harikaran <harikaran.intern@leorainfotech.in>
2023-04-17 19:42:08 +0530

    E Locker, Dropdown Design fix

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -52,7 +52,7 @@ const DropDown = (({
   return (
     <div className={`form-group`}>
       {label && <small className="form-control-label text-black ">{label}</small>}
-      <select ref={selectRef} value={value} className={`form-control mt-2  ${showArrow && "form-select"}`} {...props} onChange={handleSelectChange} name={name} disabled={isDisabled}
+      <select ref={selectRef} value={value} className={`form-control mt-2  ${showArrow && "form-select"}  ${isDisabled&&`bg-white`}`} {...props} onChange={handleSelectChange} name={name} disabled={isDisabled}
       >
         <option>{placeholder}</option>
         {data && data.length > 0 && data.map((item: { id: any; type: any; name: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | Iterable<ReactI18NextChild> | null | undefined; group_name: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | Iterable<ReactI18NextChild> | null | undefined; title: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | Iterable<ReactI18NextChild> | null | undefined; }, index: React.Key | null | undefined) => (
