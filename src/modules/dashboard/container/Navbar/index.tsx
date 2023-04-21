1e9f413353fd92e498943f06c650e509401f96cc
purushothamanzenylog <reactjs.1@zenylog.com>
2023-04-21 12:52:07 +0530

    fixes

--- src/modules/dashboard/container/Navbar/index.tsx
+++ src/modules/dashboard/container/Navbar/index.tsx
@@ -232,7 +232,7 @@ function Navbar({
             {prop.icon !== undefined ? (
               <>
                 <i className={prop.icon} />
-                <span className="nav-link-text">{prop.name}</span>
+                <span className={`${pathname === prop.path ? "sidenav-active" : "sidenav-normal "}`}>{prop.name}</span>
               </>
             ) : prop.miniName !== undefined ? (
               <>
