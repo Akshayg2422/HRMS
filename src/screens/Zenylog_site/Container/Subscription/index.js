b3808784e086465e79c0e2be32defec10da38f90
Jay <jayakumar@leorainfotech.in>
2023-09-09 19:13:47 +0530

    no message

--- src/screens/Zenylog_site/Container/Subscription/index.js
+++ src/screens/Zenylog_site/Container/Subscription/index.js
@@ -1,6 +1,5 @@
 import { useNav } from '@utils';
 import { Container } from 'reactstrap';
-import './index.css'
 import { useState, useEffect } from 'react';
 
 
@@ -15,48 +14,46 @@ function Subscription() {
   const [amountOneYear, setAmountOneYear] = useState(10);
   const [finalPayableOneYear, setFinalPayableOneYear] = useState(0);
 
-  // Calculate and update final payable for one month
-  const calculateOneMonth = () => {
-    const baseAmount = amountOneMonth * 30;
-    const gstAmount = (baseAmount * 18) / 100;
-    const finalAmount = baseAmount + gstAmount;
-    setFinalPayableOneMonth(Math.round(finalAmount));
-  };
+  {/* Use useEffect to update final payable values when input values change */ }
 
-  // Calculate and update final payable for six months
-  const calculateSixMonth = () => {
-    const baseAmount = amountSixMonth * 180;
-    const discountAmount = (baseAmount * 10) / 100;
-    const payableBeforeGST = baseAmount - discountAmount;
-    const gstAmount = (payableBeforeGST * 18) / 100;
-    const finalAmount = payableBeforeGST + gstAmount;
-    setFinalPayableSixMonth(Math.round(finalAmount));
-  };
-
-  // Calculate and update final payable for one year
-  const calculateOneYear = () => {
-    const baseAmount = amountOneYear * 360;
-    const discountAmount = (baseAmount * 25) / 100;
-    const payableBeforeGST = baseAmount - discountAmount;
-    const gstAmount = (payableBeforeGST * 18) / 100;
-    const finalAmount = payableBeforeGST + gstAmount;
-    setFinalPayableOneYear(Math.round(finalAmount));
-  };
-
-  // Use useEffect to update final payable values when input values change
   useEffect(() => {
+    {/* Calculate and update final payable for one month */ }
+    const calculateOneMonth = () => {
+      const baseAmount = amountOneMonth * 30;
+      const gstAmount = (baseAmount * 18) / 100;
+      const finalAmount = baseAmount + gstAmount;
+      setFinalPayableOneMonth(Math.round(finalAmount));
+    };
     calculateOneMonth();
   }, [amountOneMonth]);
 
   useEffect(() => {
+    {/* Calculate and update final payable for six months */ }
+    const calculateSixMonth = () => {
+      const baseAmount = amountSixMonth * 180;
+      const discountAmount = (baseAmount * 10) / 100;
+      const payableBeforeGST = baseAmount - discountAmount;
+      const gstAmount = (payableBeforeGST * 18) / 100;
+      const finalAmount = payableBeforeGST + gstAmount;
+      setFinalPayableSixMonth(Math.round(finalAmount));
+    };
     calculateSixMonth();
   }, [amountSixMonth]);
 
   useEffect(() => {
+    {/* Calculate and update final payable for one year */ }
+    const calculateOneYear = () => {
+      const baseAmount = amountOneYear * 360;
+      const discountAmount = (baseAmount * 25) / 100;
+      const payableBeforeGST = baseAmount - discountAmount;
+      const gstAmount = (payableBeforeGST * 18) / 100;
+      const finalAmount = payableBeforeGST + gstAmount;
+      setFinalPayableOneYear(Math.round(finalAmount));
+    };
     calculateOneYear();
   }, [amountOneYear]);
 
-  console.log('finalPayableSixMonthfinalPayableSixMonth',finalPayableSixMonth)
+  console.log('finalPayableSixMonthfinalPayableSixMonth', finalPayableSixMonth)
 
 
   const pricingOptions = [
@@ -72,15 +69,43 @@ function Subscription() {
       price: '₹50',
       amount: '50 JD',
       reports: ['Geo Fence Location', 'Attendance', 'Office Check-in', 'Remote Check-in'],
-      input: <div className="">
-        <input
-          type="number"
-          value={amountOneMonth}
-          onChange={(e) => setAmountOneMonth(e.target.value)}
-        />
-        <span>No of Employees</span>
-      </div>,
-      paragraph: <p>Final Payable (One Month): {finalPayableOneMonth}</p>,
+      input: (
+        <div className="input-container d-flex justify-content-between" style={{ fontSize: '12px' }}>
+          <label>
+            <h5 className="text-dark">No of Employees</h5>
+          </label>
+          <input
+            type="number"
+            value={amountOneMonth}
+            onChange={(e) => setAmountOneMonth(e.target.value)}
+            style={{ width: '50px' }}
+          />
+        </div>
+
+      ),
+      paragraph: (
+        <div className={'pt-1'} style={{ fontSize: '12px' }}>
+          <div className={'d-flex justify-content-between'}>
+            <h5>Base Amount: </h5>
+            <div>
+              <p>{`₹${amountOneMonth * 30}`}</p>
+            </div>
+          </div>
+          <div className={'d-flex justify-content-between'}>
+            <h5>GST(18%): </h5>
+            <div>
+              <p>{`₹${((amountOneMonth * 30) * 0.18).toFixed()}`}</p>
+            </div>
+          </div>
+          <div className={'d-flex justify-content-between'}>
+            <h5>Total Payable:</h5>
+            <div>
+              <p>{`₹${finalPayableOneMonth}`}</p>
+            </div>
+          </div>
+        </div>
+
+      ),
       link: `https://mockeazyprimary.leorainfotech.in/authentication/payment/?name=subscription&amount=${finalPayableOneMonth}`
     },
     {
@@ -88,15 +113,54 @@ function Subscription() {
       price: '₹300',
       amount: '300 JD',
       reports: ['Geo Fence Location', 'Attendance', 'Office Check-in', 'Remote Check-in', 'Time Sheets'],
-      input: <div className="">
-        <input
-          type="number"
-          value={amountSixMonth}
-          onChange={(e) => setAmountSixMonth(e.target.value)}
-        />
-        <span>No of Employees</span>
-      </div>,
-      paragraph: <p>Final Payable (One Month): {finalPayableSixMonth}</p>,
+      input: (
+        <div className="input-container d-flex justify-content-between" style={{ fontSize: '12px' }}>
+          <label>
+            <h5 className="text-dark">No of Employees</h5>
+          </label>
+          <input
+            type="number"
+            value={amountSixMonth}
+            onChange={(e) => setAmountSixMonth(e.target.value)}
+            style={{ width: '50px' }}
+          />
+        </div>
+      ),
+      paragraph: (
+        <div className={'pt-1'} style={{ fontSize: '12px' }}>
+          <div className={'d-flex justify-content-between'}>
+            <h5>Base Amount:</h5>
+            <div>
+              <p>{`₹${(amountSixMonth * 180).toFixed()}`}</p>
+            </div>
+          </div>
+          <div className={'d-flex justify-content-between'}>
+            <h5 className={'text-green'}>Discount 10%:</h5>
+            <div>
+              <p>{`₹${((amountSixMonth * 180 * 0.1).toFixed())}`}</p>
+            </div>
+          </div>
+          <div className={'d-flex justify-content-between'}>
+            <h5>Payable:</h5>
+            <div>
+              <p>{`₹${((amountSixMonth * 180) - (amountSixMonth * 180 * 0.1)).toFixed()}`}</p>
+            </div>
+          </div>
+          <div className={'d-flex justify-content-between'}>
+            <h5>GST(18%):</h5>
+            <div>
+              <p>{`₹${(((amountSixMonth * 180) - (amountSixMonth * 180 * 0.1)) * 0.18).toFixed()}`}</p>
+            </div>
+          </div>
+          <div className={'d-flex justify-content-between'}>
+            <h5>Total Payable:</h5>
+            <div>
+              <p>{`₹${finalPayableSixMonth}`}</p>
+            </div>
+          </div>
+
+        </div>
+      ),
       link: `https://mockeazyprimary.leorainfotech.in/authentication/payment/?name=subscription&amount=${finalPayableSixMonth}`
     },
     {
@@ -104,24 +168,64 @@ function Subscription() {
       price: '₹400',
       amount: '400 JD',
       reports: ['Geo Fence Location', 'Attendance', 'Office Check-in', 'Remote Check-in', 'Time Sheets', 'Pay Slips', 'View Timesheets'],
-      input: <div className="">
-        <input
-          type="number"
-          value={amountOneYear}
-          onChange={(e) => setAmountOneYear(e.target.value)}
-        />
-        <span>No of Employees</span>
-      </div>,
-      paragraph: <p>Final Payable (One Month): {finalPayableOneYear}</p>,
+      input: (
+        <div className="input-container d-flex justify-content-between" style={{ fontSize: '12px' }}>
+          <label>
+            <h5 className="text-dark">No of Employees</h5>
+          </label>
+          <input
+            type="number"
+            value={amountOneYear}
+            onChange={(e) => setAmountOneYear(e.target.value)}
+            style={{ width: '50px' }}
+          />
+        </div>
+      ),
+      paragraph: (
+        <div className={'pt-1'} style={{ fontSize: '12px' }}>
+          <div className={'d-flex justify-content-between'}>
+            <h5>Base Amount:</h5>
+            <div>
+              <p>{`₹${(amountOneYear * 360).toFixed()}`}</p>
+            </div>
+          </div>
+          <div className={'d-flex justify-content-between'}>
+            <h5 className={'text-green'}>Discount 25%:</h5>
+            <div>
+              <p>{`₹${((amountOneYear * 360 * 0.25).toFixed())}`}</p>
+            </div>
+          </div>
+          <div className={'d-flex justify-content-between'}>
+            <h5>Payable:</h5>
+            <div>
+              <p>{`₹${((amountOneYear * 360) - (amountOneYear * 360 * 0.25)).toFixed()}`}</p>
+            </div>
+          </div>
+          <div className={'d-flex justify-content-between'}>
+            <h5>GST(18%):</h5>
+            <div>
+              <p>{`₹${(((amountOneYear * 360) - (amountOneYear * 360 * 0.25)) * 0.18).toFixed()}`}</p>
+            </div>
+          </div>
+          <div className={'d-flex justify-content-between'}>
+            <h5>Total Payable:</h5>
+            <div>
+              <p>{`₹${finalPayableOneYear}`}</p>
+            </div>
+          </div>
+
+        </div>
+      ),
       link: `https://mockeazyprimary.leorainfotech.in/authentication/payment/?name=subscription&amount=${finalPayableOneYear}`
     },
+
   ];
 
   return (
     <Container fluid>
       <section style={{ backgroundColor: '#ffffff' }} className="pb-5" id="pricing-now-ui">
 
-        <h1 className="card-header text-website-primary">Contact{' '}<u>us</u></h1>
+        <h1 className="card-header text-website-primary">Our <u>Pricing</u></h1>
         <div className=" position-relative">
           <div className="container pb-lg-8 pb-7 pt-4 postion-relative z-index-2 position-relative">
             <div className="row">
@@ -143,7 +247,6 @@ function Subscription() {
                   <div className="card h-100">
                     <div className={'text-sm-start text-center pt-4 pb-3 px-4'}>
                       <p className="font-weight-bold">{option.title}</p>
-                      {/* <h2 className="font-weight-bold mt--2">{option.price}</h2> */}
                     </div>
                     <hr className={`horizontal ${index % 2 === 0 ? 'light' : 'dark'} my-0`} />
                     <div className={'col'}>
@@ -173,11 +276,11 @@ function Subscription() {
                                             <img className={'mb-1'} src={Icons} alt="..." height={60} width={60} />
                                             <p className="mb-1">{option.amount}</p>
                                         </div> */}
-                      <div className={''}>{option.input}</div>
-                      <div className={'text-black'}>{option.paragraph}</div>
+                      <div className={'input-container'}>{option.input}</div>
+                      <div className={'text-dark'}>{option.paragraph}</div>
                       {option.price === 'FREE' ? (
                         <div
-                          className="custom-btn custom-color  border-0 col mt-1"
+                          className="custom-color border-0 col mt-1"
 
                         // onClick={option.onClick}
                         >
@@ -195,7 +298,7 @@ function Subscription() {
                             width: '100%',
                           }}
                         >
-                          <div className="custom-btn custom-color text-center text-white border-0 col mt-1">
+                          <div className="custom-color text-center text-white border-0 col mt-1">
                             Buy Now
                           </div>
                         </a>
